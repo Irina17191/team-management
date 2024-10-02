be75f8e64f55caf7d94707465918d9f979aad291
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-10-02 18:16:33 +0300

    Create Team and Person models with ManyToMany relationship

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Person(models.Model):
+    """
+    Represents a person who can belong to multiple teams.
+
+    The ManyToManyField is used to allow a person to belong to multiple teams,
+    reflecting real-world scenarios where an individual, such as a manager,
+    can participate in both the administration and development teams.
+    """
+
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    teams = models.ManyToManyField(Team, blank=True, related_name="persons")
+
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
+
+    class Meta:
+        ordering = ["name"]
