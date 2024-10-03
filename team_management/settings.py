45ffcdf8c6e2fe49ee260a1fb61a7f8dba4ff416
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-10-03 19:52:49 +0300

    Add Swagger documentation, including custom view details

--- team_management/settings.py
+++ team_management/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "teams",
     "rest_framework",
     "debug_toolbar",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,20 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Team management API",
+    "DESCRIPTION": "Manage teams and team members",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelsExpandDepth": 2,
+        "defaultModelExpandDepth": 2,
+    },
+}
--- teams/views.py
+++ teams/views.py
@@ -1,13 +1,38 @@
 from rest_framework import viewsets
+
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+
 from teams.models import Team, Person
 from teams.serializers import TeamSerializer, PersonSerializer
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.prefetch_related("teams").all()
+    serializer_class = PersonSerializer
 
 
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.prefetch_related("persons").all()
     serializer_class = TeamSerializer
 
+    def get_queryset(self):
+        name = self.request.query_params.get("name")
+        queryset = self.queryset
 
-class PersonViewSet(viewsets.ModelViewSet):
-    queryset = Person.objects.prefetch_related("teams").all()
-    serializer_class = PersonSerializer
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset.distinct()
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "name",
+                type=OpenApiTypes.STR,
+                description="Filter team by name (ex. ?name=developer)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
